caa02f7ec23cde54b286942e84d54829ad8ec075
Vladimir Tkach <teamco@gmail.com>
2011-07-03 17:47:38 +0300

    First project commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Idogu::Application.config.secret_token = '0252a07c7c02f2faab6577b632fea6b8ac2f1210e8453026c59ed00b5239732825c43785e7d3a092e5427b13340d09e1916aeced03ccf50d24215f80f1551d76'
