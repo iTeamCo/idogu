a2efd6fdb59820ed1af6b9a8f91233e629e3e482
Vladimir Tkach <teamco@gmail.com>
2011-07-08 14:18:15 +0300

    add generic form helper add actions_list helper (show/edit/destroy)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
-  def render_error(variable)
-    if variable.errors.any?
+  def render_error(instance)
+    if instance.errors.any?
       content_tag :div, :id => 'error_explanation' do
-        content_tag(:h2, pluralize(variable.errors.count, 'error') << ' prohibited this user from being saved:')+
-        content_tag(:ul, raw(variable.errors.full_messages.map { |msg| content_tag :li, msg }.join))
+        content_tag(:h2, pluralize(instance.errors.count, 'error') << ' prohibited this user from being saved:')+
+        content_tag(:ul, raw(instance.errors.full_messages.map { |msg| content_tag :li, msg }.join))
       end
     end
   end
@@ -13,4 +13,27 @@ module ApplicationHelper
       f.submit
     end
   end
+
+  def tag_helper(f, type, field)
+    content_tag :div, :class => "field" do
+      f.label(field)  <<
+      raw('<br />')   <<
+      f.send(type, field)
+    end
+  end
+
+  def action_list(instance, edit_path)
+    content_tag(:td) { link_to 'Show', instance }   <<
+    content_tag(:td) { link_to 'Edit', edit_path }  <<
+    content_tag(:td) { link_to 'Destroy', instance, confirm: 'Are you sure?', method: :delete }
+  end
+
+  def render_form(instance, opts={})
+    form_for(instance) do |f|
+      raw(render_error instance) <<
+      raw(opts.map{|k,v| tag_helper f, v, k }.join) <<
+      submit_form(f)
+    end
+  end
+
 end
